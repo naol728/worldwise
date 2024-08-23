f6359940ba6d11d5f94ae4b5e051e8c247b4095e
naol <naolmeseret22@gmail.com>
2024-08-23 23:59:20 +0300

    i define the base url outside from the componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useEffect, useState } from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Pricing from './pages/Pricing'
@@ -8,7 +8,23 @@ import PageNotFound from './pages/PageNotFound';
 import Login from './pages/Login';
 import CityList from './components/CityList';
 import './index.css'
+import { useState } from 'react';
+const BASE_URL="http://localhost:8000";
 export default function App() {
+ 
+  const [cities,setCities]=useState({})
+  const [isloading,setIsloading]=useState(false)
+
+  useEffect(
+    function (){
+     async function fechcites(){
+        const res =await fetch(`${BASE_URL}/cities`)
+        const data=await res.json();
+        setCities(data)
+      }
+      fechcites();
+    },[cities]
+  )
   return ( 
     <div>
     <BrowserRouter>
