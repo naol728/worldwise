bb53619270160dfc6a78ef367fd77e7b5f21ef6f
naol <naolmeseret22@gmail.com>
2024-08-24 22:54:41 +0300

    i fix some componet

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,44 @@ import CityList from './components/CityList';
 import './index.css'
 // const BASE_URL="http://localhost:8000/cities";
 export default function App() {
- 
-  const [cities,setCities]=useState([])
-  const [isloading,setIsloading]=useState(false)
-  console.log(isloading)
+  const [cities,setCities]=useState([{
+    "cityName": "Lisbon",
+    "country": "Portugal",
+    "emoji": "🇵🇹",
+    "date": "2027-10-31T15:59:59.138Z",
+    "notes": "My favorite city so far!",
+    "position": {
+      "lat": 38.727881642324164,
+      "lng": -9.140900099907554
+    },
+    "id": "73930385"
+  },
+  {
+    "cityName": "Madrid",
+    "country": "Spain",
+    "emoji": "🇪🇸",
+    "date": "2027-07-15T08:22:53.976Z",
+    "notes": "",
+    "position": {
+      "lat": 40.46635901755316,
+      "lng": -3.7133789062500004
+    },
+    "id": "17806751"
+  },
+  {
+    "cityName": "Berlin",
+    "country": "Germany",
+    "emoji": "🇩🇪",
+    "date": "2027-02-12T09:24:11.863Z",
+    "notes": "Amazing 😃",
+    "position": {
+      "lat": 52.53586782505711,
+      "lng": 13.376933665713324
+    },
+    "id": "98443197"
+  }])
+  const [isLoading ,setIsloading]=useState(false)
+  console.log(isLoading )
   useEffect(
     function (){
      async function fechcites(){
@@ -34,7 +68,7 @@ export default function App() {
       fechcites();
     },[]
   )
-  console.log(cities)
+
   return ( 
     <div>
     <BrowserRouter>
@@ -44,8 +78,8 @@ export default function App() {
          <Route path='/product' element={<Product   />}/>
          <Route path='/login' element={<Login   />}/>
          <Route path='/app' element={<AppLayout />}>
-            <Route index element={<CityList isloading={isloading} cities={cities} />}/>
-            <Route path='cites' element={<CityList isloading={isloading} cities={cities} />} />
+            <Route index element={<CityList cities={cities}  isLoading ={isLoading } />}/>
+            <Route path='cites' element={<CityList  cities={cities} isLoading ={isLoading } />} />
             <Route path='countries' element={<p>list of counties</p>} />
             <Route path='form' element={<p>form</p>} />
          </Route>
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,12 +1,24 @@
-import style from './CityList.module.css'
-import Spinner from './Spinner'
-import Cityitem from './Cityitem'
-export default function CityList(cities,isloading) {
-  console.log(cities)
-  if(isloading) return <Spinner />
+import Spinner from "./Spinner";
+import styles from "./CityList.module.css";
+import CityItem from "./CityItem";
+import Message from "./Message";
+
+function CityList({cities,isLoading} ) {
+
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
   return (
-    <ul className={style.citylist}>
-      {cities.map((city)=> <Cityitem city={city} key={city.id} />)}
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
     </ul>
   );
 }
+
+export default CityList;
