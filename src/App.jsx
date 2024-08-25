13272febbd2049391799b175d1d294bb1a800fd6
naol <naolmeseret22@gmail.com>
2024-08-25 23:03:16 +0300

    i add countrylist componets

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
 import Login from './pages/Login';
 import CityList from './components/CityList';
+import CountryList from './components/CountryList';
 import './index.css'
 // const BASE_URL="http://localhost:8000/cities";
 export default function App() {
@@ -43,7 +44,7 @@ export default function App() {
          <Route path='/app' element={<AppLayout />}>
             <Route index element={<CityList cities={cities}  isLoading ={isLoading } />}/>
             <Route path='cites' element={<CityList  cities={cities} isLoading ={isLoading } />} />
-            <Route path='countries' element={<p>list of counties</p>} />
+            <Route path='countries' element={<CountryList cities={cities} isLoading ={isLoading } />} />
             <Route path='form' element={<p>form</p>} />
          </Route>
          <Route path='/*' element={<PageNotFound />}/>
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,30 @@
+import Spinner from "./Spinner";
+import styles from "./CountryList.module.css";
+import Message from "./Message";
+import CountryItems from './CountryItem'
+
+function CountryList({cities,isLoading}) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length){
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    )}
+    const country=cities.reduce((arr,city)=>{
+        if(!arr.map(e=>e.city).includes(city.country))
+           return [
+        ...arr,{country:city.country,emoji:city.emoji}] 
+        else return arr;
+        },[])
+        
+  return (
+    <ul className={styles.countryList}>
+      {country.map((country)=>
+        <CountryItems country={country} key={country.id} />
+        
+      )   }
+    </ul>
+  );
+} 
+
+
+export default CountryList;
