50798def2c8c7755a87e9a0327545e5d65b9e588
naol <naolmeseret22@gmail.com>
2024-08-30 23:15:57 +0300

    i change the usestate hook in the cites context to usereducer

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,48 +1,100 @@
-import { createContext, useContext, useEffect, useState } from "react"
+import { act } from "react"
+import { createContext, useContext, useEffect, useReducer, useState } from "react"
 const CitesContext=createContext()
+
+const initalstate={
+  cities:[],
+  isLoading:false,
+  currentcity:{},
+  error:""
+}
+
+function reducer(state,action){
+  switch(action.type){
+    case "Loading":
+      return {
+        ...state,
+        isLoading:true
+      }
+     case "cities/loaded":
+     return {
+       ...state,
+       isLoading:false,
+       cities:action.payload
+      }
+      case "city/loaded":
+        return{
+          ...state,
+          isLoading:false,
+          currentcity:action.payload
+        }
+      case "city/created":
+        return{
+          ...state,
+          isLoading:false,
+          cities:(cities)=>[...cities,action.payload]
+        }
+      case "city/deleted":
+        return{
+          ...state,
+          isLoading:false,
+          cities:(cite)=>cite.filter( (city)=>city.id!==action.payload)
+        }
+      case "error":
+       return {
+        ...state,
+        isLoading:false,
+         error:alert(action.payload)
+        }
+    default :throw new Error("unkown event")
+  }
+}
+
 function CitiesProvider({children}) {
-  const [cities,setCities]=useState([])
-  const [isLoading ,setIsloading]=useState(false)
-   const [currentcity,setCurrentcity]=useState({})
+
+   const [{  cities,
+    isLoading,
+    currentcity,
+    error,},dispach]=useReducer(reducer,initalstate)
+  // const [cities,setCities]=useState([])
+  // const [isLoading ,setIsloading]=useState(false)
+  //  const [currentcity,setCurrentcity]=useState({})
   
   
   useEffect(
     function (){
      async function fechcites(){
+      dispach({type:"Loading"})
        try { 
-        setIsloading(true)
         const res =await fetch(`http://localhost:8000/cities`)
         const data=await res.json();
-        setCities(data)}
+         dispach({type:"cities/loaded",payload:data})
+      }
         catch{
-          alert("there is an error loading the data")
-        }
-        finally{
-          setIsloading(false)
+          dispach({type:"error" ,payload:"there is an error loading the data"})
         }
       }
-     
       fechcites();
     },[]
   )
   
   async function getcity(id){
+    dispach({type:"Loading"})
     try { 
-     setIsloading(true)
+   
      const res =await fetch(`http://localhost:8000/cities/${id}`)
      const data=await res.json();
-     setCurrentcity(data)}
+     dispach({type:"city/loaded" ,payload:data})
+    
+    }
      catch{
-       alert("there is an error loading the data")
-     }
-     finally{
-       setIsloading(false)
+      dispach({type:"error",payload:"there is an error while loading the city data "})
      }
    }
 
   async function createnewcity(newCity){
+    dispach({type:"Loading"})
     try { 
-     setIsloading(true)
      const res =await fetch(`http://localhost:8000/cities/`,{
       method: "POST",
       body: JSON.stringify(newCity),
@@ -51,31 +103,24 @@ function CitiesProvider({children}) {
       },
      })
      const data=await res.json();
-     setCities((cities)=>[...cities,data])
+      dispach({type:"city/created",payload:data})
     
     }
      catch{
-       alert("there is an error CREATING the CITY")
-     }
-     finally{
-       setIsloading(false)
+      dispach({type:"error",payload:"there is an error while creating a new city"})
      }
    }
   async function deletecity(id){
+    dispach({type:"Loading"})
     try { 
-     setIsloading(true)
       await fetch(`http://localhost:8000/cities/${id}`,{
       method: "DELETE",
      })
-    
-     setCities((cite)=>cite.filter( (city)=>city.id!==id))
+    dispach({type:"city/deleted",payload:id})
 
     }
      catch{
-       alert("there is an error DELETING the CITY")
-     }
-     finally{
-       setIsloading(false)
+      dispach({type:"error",payload:"there is an error while deleting a city"})
      }
    }
   
