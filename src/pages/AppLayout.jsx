4c0dcd31e406faa7e366d4114af6e5389d27daf5
naol <naolmeseret22@gmail.com>
2024-08-31 01:52:25 +0300

    i add  a logout functionality to the app

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,13 @@
 import SideBar from "../components/SideBar";
 import style from '../pages/AppLayout.module.css'
 import Map from "../components/Map";
+import User from '../components/User'
 export default function AppLayout() {
   return (
     <div className={style.app} >
       <SideBar />
       <Map />
+      <User />
     </div>
   )
 }
