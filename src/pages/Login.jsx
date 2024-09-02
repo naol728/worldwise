a2161fdb78762da3e5e55e06f251aa2838b1ee06
naol <naolmeseret22@gmail.com>
2024-09-03 00:50:10 +0300

    i change the loginpage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,36 @@
-import styles from "./Login.module.css";
-import PageNav from '../components/PageNav'
 import { useEffect, useState } from "react";
-import style from '../components/PageNav.module.css'
-import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
+import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
+import styles from "./Login.module.css";
+
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
-  const {login,isAuthenticated}=useAuth()
-   const navigate=useNavigate()
-   function handlelogin(e){
-  e.preventDefault();
-    if(email && password) login(email,password);
-   }
-   useEffect(
-    function(){
-      if(isAuthenticated===true) navigate("/app")
-    },[isAuthenticated,navigate]
-  )
 
-  
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (email && password) login(email, password);
+  }
+
+  useEffect(
+    function () {
+      if (isAuthenticated) navigate("/app", { replace: true });
+    },
+    [isAuthenticated, navigate]
+  );
+
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form} onSubmit={handlelogin}>
+
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -47,7 +52,7 @@ export default function Login() {
         </div>
 
         <div>
-          <Button type="primary">log in</Button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
